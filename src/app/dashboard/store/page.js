2d8cbf35ef92245248df9d68aba216ef66f37976
Adefeyitimi Adeyeloja <33554538+TimmyIsANerd@users.noreply.github.com>
2024-12-31 02:05:01 +0100

    feat: enhance StorePage with modal for new storefront creation

--- src/app/dashboard/store/page.js
+++ src/app/dashboard/store/page.js
@@ -1,12 +1,33 @@
-export default function StorePage(){
-    return (
-        <>
-            <div className="w-full my-3 p-3">
-                <div>
-                    <h3 className="font-medium text-3xl">Store</h3>
-                    <p></p>
-                </div>
-            </div>
-        </>
-    )
+"use client";
+import { useState } from "react";
+import StoreHeader from "@/components/Store/StoreFront/Filter";
+import StoreTable from "@/components/Store/StoreFront/Table";
+import NewStorefrontModal from "@/components/Store/StoreFront/NewStoreModal";
+
+export default function StorePage() {
+  const [openNewStoreFrontModal, setOpenNewStoreFrontModal] = useState(false);
+
+  async function handleNewStorefront(formData) {
+    console.log("Create new store front with data:", formData);
+    setOpenNewStoreFrontModal(false);
+  }
+
+  return (
+    <>
+      <div className="w-full my-3 p-3">
+        <div>
+          <h3 className="font-medium text-3xl">Store Fronts</h3>
+          <p></p>
+        </div>
+        <StoreHeader openStore={() => setOpenNewStoreFrontModal(true)} />
+        <StoreTable />
+        {openNewStoreFrontModal && (
+          <NewStorefrontModal
+            onSubmit={handleNewStorefront}
+            onClose={() => setOpenNewStoreFrontModal(false)}
+          />
+        )}
+      </div>
+    </>
+  );
 }
