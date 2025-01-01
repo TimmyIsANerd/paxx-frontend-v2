b7b21e2260a097c6fbaf8d72f71886e3e4f24cbd
Adefeyitimi Adeyeloja <33554538+TimmyIsANerd@users.noreply.github.com>
2025-01-01 21:15:59 +0100

    refactor: streamline middleware logic for authentication checks on login and dashboard routes

--- src/middleware.js
+++ src/middleware.js
@@ -5,27 +5,27 @@ const baseURL =
     : "http://localhost:3000";
 
 export function middleware(request) {
-  // const url = request.url;
-  // if (url.includes("/login") || url.includes("/signup")) {
-  //   const cookie = request.cookies.get("paxx_user");
+  const url = request.url;
+  if (url.includes("/login") || url.includes("/signup")) {
+    const cookie = request.cookies.get("paxx_user");
 
-  //   if (cookie) {
-  //     console.log(
-  //       `Checking For Cookie on Auth Page`,
-  //       cookie ? "Cookie Found 🍪" : "No Cookie ❌"
-  //     );
-  //     return NextResponse.redirect(`${baseURL}/dashboard`);
-  //   }
-  // }
+    if (cookie) {
+      console.log(
+        `Checking For Cookie on Auth Page`,
+        cookie ? "Cookie Found 🍪" : "No Cookie ❌"
+      );
+      return NextResponse.redirect(`${baseURL}/dashboard`);
+    }
+  }
 
-  // if (url.includes("/dashboard")) {
-  //   const cookie = request.cookies.get("paxx_user");
-  //   if (!cookie) {
-  //     console.log(
-  //       `Checking For Cookie on Dashboard Page`,
-  //       cookie ? "Cookie Found 🍪" : "No Cookie ❌"
-  //     );
-  //     return NextResponse.redirect(`${baseURL}/auth/login`);
-  //   }
-  // }
+  if (url.includes("/dashboard")) {
+    const cookie = request.cookies.get("paxx_user");
+    if (!cookie) {
+      console.log(
+        `Checking For Cookie on Dashboard Page`,
+        cookie ? "Cookie Found 🍪" : "No Cookie ❌"
+      );
+      return NextResponse.redirect(`${baseURL}/auth/login`);
+    }
+  }
 }
