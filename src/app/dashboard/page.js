a15e2b219ac1be2d04a7cac11cb4b54a6077c235
Adefeyitimi Adeyeloja <33554538+TimmyIsANerd@users.noreply.github.com>
2025-01-01 22:16:06 +0100

    feat: enhance DashboardHome by adding TransactionHistory component and improving layout for better user experience

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -4,15 +4,16 @@ import { getFirstName } from "@/utils/lib";
 import Link from "next/link";
 import DashboardStats from "@/components/Dashboard/Stats";
 import OnboardingSection from "@/components/Dashboard/Onboarding";
+import TransactionHistory from "@/components/Dashboard/TransactionHistory";
 
 export default function DashboardHome() {
   const { user } = useAuth();
   return (
-    <div className="w-full my-3 p-3">
+    <div className="w-full my-3 p-3 max-h-screen overflow-y-auto">
       <OnboardingSection
         firstName={user ? getFirstName(user.fullName) : "..."}
       />
-      {/* <DashboardStats /> */}
+      <TransactionHistory />
     </div>
   );
 }
