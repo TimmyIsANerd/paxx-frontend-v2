2f83912659d06ae826ac218c886ea0d859fbc3d9
Adefeyitimi Adeyeloja <33554538+TimmyIsANerd@users.noreply.github.com>
2025-01-02 21:59:55 +0100

    chore: updated endpoints

--- src/services/auth.js
+++ src/services/auth.js
@@ -27,3 +27,26 @@ export async function register(payload) {
     throw error;
   }
 }
+
+export async function resendOTPCall(payload) {
+  try {
+    const response = await axios.post(`${baseURL}/api/v1/auth/resend`, payload);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+export async function verifyOTPCall(payload) {
+  try {
+    const response = await axios.post(
+      `${baseURL}/api/v1/auth/verify/email`,
+      payload
+    );
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
