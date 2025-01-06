615f198891ae2d61a5bb82fd7f605d79f106e953
Adefeyitimi Adeyeloja <33554538+TimmyIsANerd@users.noreply.github.com>
2025-01-06 09:42:34 +0100

    fix: removed extra toast container

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -1,6 +1,5 @@
 import Navbar from "@/components/Dashboard/Navbar";
 import Sidebar from "@/components/Dashboard/Sidebar";
-import { ToastContainer } from "react-toastify";
 import { WalletContextProvider } from "@/context/WalletContext";
 
 export const metadata = {
@@ -12,14 +11,6 @@ export default function DashboardLayout({ children }) {
     <WalletContextProvider>
       <main className="max-h-screen">
         <Navbar />
-        <ToastContainer
-          toastClassName="bg-black text-[#005BFE] border-2 border-[#005BFE] rounded-md shadow-2xl shadow-[#005BFE]"
-          bodyClassName="font-semibold text-center md:text-left text-sm text-black"
-          hideProgressBar={true}
-          closeButton={false}
-          position="top-right"
-          closeOnClick={true}
-        />
         <div className="flex items-start">
           <Sidebar />
           {children}
