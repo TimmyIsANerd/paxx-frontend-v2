76157acf190607408495ddcadd45b0c8bf74ad07
Adefeyitimi Adeyeloja <33554538+TimmyIsANerd@users.noreply.github.com>
2025-01-19 10:29:07 +0100

    feat: update base URL configuration for store and payment services
    - Changed the local development URL for payBaseUrl from http://localhost:3003 to http://localhost:3002.
    - Added storeBaseUrl configuration to manage the base URL for the store service, with production and local environment settings.
    - This update enhances the application's flexibility in handling different service endpoints based on the environment.

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,8 @@
 export const payBaseUrl =
   process.env.NODE_ENV === "production"
     ? "https://pay.usepaxx.xyz"
-    : "http://localhost:3003";
+    : "http://localhost:3002";
+
+export const storeBaseUrl = 
+  process.env.NODE_ENV === "production"
+    ? "https://store.usepaxx.xyz" : "http://localhost:3001"
